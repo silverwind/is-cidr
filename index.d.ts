edf039281c3615041ac3b8b0664a993e8cff02c9
silverwind <me@silverwind.io>
2024-04-09 08:26:30 +0200

    Fix typescript definitions for properties on default export
    Fixes: https://github.com/silverwind/is-cidr/issues/9

--- index.d.ts
+++ index.d.ts
@@ -12,9 +12,14 @@ Test if `input` is a IPv6 CIDR.
 */
 export function v6(str: string): boolean;
 
-/**
-Test if `input` is a IPv4 or IPv6 CIDR.
+declare const _default: {
+  /**
+  Test if `input` is a IPv4 or IPv6 CIDR.
 
-@returns Either `4`, `6` (indicating the IP version) or `0` if the string is not a CIDR.
-*/
-export default function isCidr(str: string): 6 | 4 | 0;
+  @returns Either `4`, `6` (indicating the IP version) or `0` if the string is not a CIDR.
+  */
+  (str: string): 6 | 4 | 0,
+  v4: typeof v4;
+  v6: typeof v6;
+};
+export default _default;
--- index.test-d.ts
+++ index.test-d.ts
@@ -4,3 +4,5 @@ import isCidr, {v4, v6} from "./index.js";
 expectType<6 | 4 | 0>(isCidr("foo"));
 expectType<boolean>(v4("foo"));
 expectType<boolean>(v6("foo"));
+expectType<boolean>(isCidr.v4("foo"));
+expectType<boolean>(isCidr.v6("foo"));
